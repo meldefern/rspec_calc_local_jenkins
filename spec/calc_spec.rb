303ef926cbfa40801a965b3ad86e127755ac45e0
Melinda Fernandes <melindafernandes1993@gmail.com>
2017-10-05 12:29:58 +0100

    initial commit

---
+++ spec/calc_spec.rb
@@ -0,0 +1,40 @@
+
+describe Calculator do
+
+	before(:each) do
+		@calc = Calculator.new
+	end
+
+	it "should have an add method" do
+		expect(@calc).to respond_to (:add)
+	end
+
+	it "should have an minus method" do
+		expect(@calc).to respond_to (:minus)
+	end
+
+	it "should have an divide method" do
+		expect(@calc).to respond_to (:divide)
+	end
+
+	it "should have an multiply method" do
+		expect(@calc).to respond_to (:multiply)
+	end
+
+	it "should add two numbers" do
+		expect(@calc.add(2,2)).to eq 4
+	end
+
+	it "should subtract two numbers" do
+		expect(@calc.minus(2,2)).to eq 1
+	end
+
+	it "should divde two numbers" do
+		expect(@calc.divide(2,2)).to eq 2
+	end
+
+	it "should multiply two numbers" do
+		expect(@calc.multiply(2,2)).to eq 4
+	end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+require_relative '../calc'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
